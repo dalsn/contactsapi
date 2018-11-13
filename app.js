a8177d4c4eb672c828ff0303f3b79b673ede108b
Dalhatu <kulgee001@yahoo.com>
2018-11-13 16:36:43 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+
+const SUCCESS = 200;
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger("dev"));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": false}));
+
+app.get("*", (req, res) => res.status(SUCCESS).send({
+    "message": "Welcome to the beginning of nothingness.",
+}));
+
+module.exports = app;
